7828fe608e91016c19633cf81d0edb0fb1d486a9
musadevsinn <musadevsinn@gmail.com>
2024-08-14 22:30:29 +0500

    change in styles

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// Function to check if an element is in the viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Function to add the .in-view class to elements in the viewport
-function addInViewClass() {
-    const animatedElements = document.querySelectorAll('.animated');
-    animatedElements.forEach((element) => {
-        if (isInViewport(element)) {
-            element.classList.add('in-view');
-        }
-    });
-}
-
-// Run the function on scroll and on initial load
-window.addEventListener('scroll', addInViewClass);
-window.addEventListener('load', addInViewClass);
+new WOW({
+    boxClass: 'wow',      // default
+    animateClass: 'animated', // default
+    offset: 0,           // default
+    mobile: true,        // default
+    live: true           // default
+  }).init();
+  
