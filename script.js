43a965d55e2c7d38e91e9b0f3c26f93ca1140ac1
musadevsinn <musadevsinn@gmail.com>
2024-08-15 22:42:46 +0500

    change script

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
-let slideIndex = 0;
-const slideCards = document.querySelector('.slide-cards');
-
-function moveSlide(n) {
-    slideIndex += n;
-    showSlides();
-}
-
-function showSlides() {
-    const slides = document.querySelectorAll('.slide-card');
-    const totalSlides = slides.length;
-
-    if (slideIndex >= totalSlides - 2) {
-        slideIndex = totalSlides - 2; // Restrict sliding beyond the last card
-    } else if (slideIndex < 0) {
-        slideIndex = 0; // Restrict sliding before the first card
-    }
-
-    slideCards.style.transform = `translateX(-${slideIndex * 351}px)`;
-}
-
-// Optional: Stop auto-sliding when user interacts with the buttons
-let autoSlide = setInterval(() => {
-    slideIndex++;
-    showSlides();
-}, 5000);
-
-document.querySelector('.prev').addEventListener('click', () => {
-    clearInterval(autoSlide);
-    autoSlide = setInterval(() => {
-        slideIndex++;
-        showSlides();
-    }, 5000);
-});
-
-document.querySelector('.next').addEventListener('click', () => {
-    clearInterval(autoSlide);
-    autoSlide = setInterval(() => {
-        slideIndex++;
-        showSlides();
-    }, 5000);
-});
+new WOW({
+    boxClass: 'wow',      // default
+    animateClass: 'animated', // default
+    offset: 0,           // default
+    mobile: true,        // default
+    live: true           // default
+  }).init();
